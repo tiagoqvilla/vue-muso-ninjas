6672ef16bba6214b719726650ac29bcc72d394ae
Tiago Villa <tiago_villa@hotmail.com>
2022-10-08 19:15:46 -0300

    Add delete storage photo function
    + update storage rules

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -9,6 +9,7 @@
             <h2>{{playlist.title}}</h2>
             <p class="username">Created by {{playlist.userName}}</p>
             <p class="description">{{playlist.description}}</p>
+            <button v-if="ownership" @click="handleDelete">Delete playlist</button>
         </div>
 
         <!-- song list -->
@@ -19,14 +20,34 @@
 </template>
 
 <script>
+    import useDocument from '@/composables/useDocument'
+    import useStorage from '@/composables/useStorage';
     import getDocument from '@/composables/getDocument';
+    import getUser from '@/composables/getUser';
+    import { computed } from 'vue';
+    import { useRouter } from 'vue-router';
 
     export default {
         props: ['id'],
         setup(props) {
             const {document: playlist, error} = getDocument('playlists', props.id)
+            const {user} = getUser()
+            const {deleteDoc} = useDocument('playlists', props.id)
+            const {deleteImage} = useStorage()
+            const router = useRouter()
 
-            return {playlist, error}
+            const ownership = computed(() => {
+                return playlist.value && user.value && user.value.uid === playlist.value.userId
+            })
+
+            const handleDelete = async () => {
+                await deleteImage(playlist.value.filePath)
+                await deleteDoc()
+                router.push({name: 'Home'})
+
+            }
+
+            return {playlist, error, ownership, handleDelete}
         }
     }
 </script>
