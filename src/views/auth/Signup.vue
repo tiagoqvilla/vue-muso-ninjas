fdceec17c5c8b8fb3c8ddccc682f1017af7ff04e
Tiago Villa <tiago_villa@hotmail.com>
2022-10-22 19:51:53 -0300

    Add UserPlaylists component

--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -13,11 +13,13 @@
 <script>
     import useSignup from '@/composables/useSignup'
     import {ref} from 'vue'
+    import {useRouter} from 'vue-router'
 
     export default {
         setup() {
 
             const {error, signup, isPending} = useSignup()
+            const router = useRouter()
 
             const email = ref('')
             const password = ref('')
@@ -27,7 +29,7 @@
                 const res = await signup(email.value, password.value, displayName.value)
 
                 if(!error.value) {
-                    console.log('user signed up');
+                    router.push({name: 'UserPlaylists'})
                 }
             }
 
---
+++ src/views/playlists/UserPlaylists.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="user-playlists">
+        <h2>My Playlists</h2>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
